c7a05512c769cae79a839627653dcab990f73e16
Karoline Krogsbøll <you@example.com>
2019-05-24 01:35:04 +0200

    Nav virker men uden links

---
+++ script.js
@@ -0,0 +1,30 @@
+let dropbtn = document.querySelector(".dropbtn");
+let dropcontent = document.querySelector(".dropdown-content");
+
+let menu = document.querySelector("#menu");
+let overlay = document.querySelector(".overlay");
+let overlaycontent = document.querySelector(".overlaycontent");
+let exit = document.querySelector(".closebtn");
+
+menu.addEventListener("click", showOverlay);
+
+function showOverlay() {
+    overlay.style.width = "100%";
+    overlaycontent.style.display = "block";
+    exit.style.display = "block";
+
+    exit.addEventListener("click", closeOverlay);
+
+}
+
+function closeOverlay() {
+    overlay.style.width = "0%";
+    overlaycontent.style.display = "none";
+    exit.style.display = "none";
+}
+
+dropbtn.addEventListener("click", dropDown);
+
+function dropDown() {
+    dropcontent.classList.toggle("show");
+}
